48cac3a6b037c024d5c33a57d87be19fe8e03fe8
Gutierrez Iniguez <maximo.gutierrez.iniguez@intel.com>
2025-10-20 11:05:26 -0600

    Started the overloading and operator psection (19)

--- polymorphism/overriding.py
+++ polymorphism/overriding.py
@@ -4,14 +4,31 @@ class Employee:
 
     def get_number_of_working_hours(self):
         return self.number_of_working_hours 
-employee1 = Employee()
-employee1.set_number_of_working_hours()
-print("Number of working hours of employee: ",)
+    
+class Trainee(Employee):
+    def set_number_of_working_hours(self):
+        self.number_of_working_hours = 45
 
+    #You can override functions when you inherit, but also, you can acceed to the function of the father if you need to, using the super() method
+    def reset_number_of_working_hours(self):
+        super().set_number_of_working_hours()
 
-#You can override functions when you inherit, but also, you can acceed to the function of the father if you need to, using the super() method
+trainee1 = Trainee()
+trainee1.set_number_of_working_hours()
+print("Number of working hours from the employee:", trainee1.get_number_of_working_hours())
+trainee1.reset_number_of_working_hours()
+print("Number of working hours from the employee:", trainee1.get_number_of_working_hours())
+
+#employee1 = Employee()
+#employee1.set_number_of_working_hours()
+#trainee1.set_number_of_working_hours()
+#print("Number of working hours from the employee:", employee1.get_number_of_working_hours())
+#print("Number of working hours from the trainee:", trainee1.get_number_of_working_hours())
+#
+
 
 
 class GovermentSale():
+    __cost = 0.16
     def calculate_extras():
         print("Hellos")
