ed0ef3d8b484feb33922b62b53113413c4dfa009
Gutierrez Iniguez <maximo.gutierrez.iniguez@intel.com>
2025-10-21 12:30:14 -0600

    Finished the course

--- final_project/classes.py
+++ final_project/classes.py
@@ -1,15 +1,62 @@
-import abstract
+import abstract, random
 class SavingsAccount(abstract.Schema):
    def __init__(self, user_name, balance):
       self.user_name = user_name
       self.balance = balance
+      account_number = self.create_account_number()
+      self.account_number = account_number
+      print("Account created succesfully!", end=' ')   
+      print("Account number: ", account_number)
+      print("Initial balance: $", balance)
+
+   def info(self):
+      print("Account: ", self.account_number)
+      print("Owner: ", self.user_name)
+      print("Balance: $", self.balance)
+   @staticmethod
+   def create_account_number():
+    return random.randint(10000, 99999)
+   def deposit(self, quantity):
+      self.balance += quantity
 
 class User(abstract.Schema):
-   __accounts = []
    def __init__(self, name):
       self.name = name
-class DataBase(abstract.Schema):
-   def __init__(self):
+      self.accounts = []
+   def info(self):
+      print("User details")
+      print("Name: ", self.name)
+      print("Accounts ({}) :".format(len(self.accounts)))
+      total_money=0
+      for account in self.accounts:
+         account.info()
+         total_money += account.balance
+      print("Total money: $", total_money)
+   def create_account(self, initial_deposit):
+      account = SavingsAccount(self.name, initial_deposit)
+      self.accounts.append(account)
+      print("Print account created succesfully!")
+      account.info()
+   def get_accounts(self):
+      return self.accounts
+
+class Database(abstract.Schema):
+   def __init__(self, users):
+      self._users = users
+   def info(self):
+      if not self._users:
+         print("There are no users in the database.")
+      else:
+         print("Database information:")
+         for user in self._users:
+            user.info()
+   def get_users(self):
+      return self._users
+               
+            
+
+
+
       
 
 
--- final_project/main.py
+++ final_project/main.py
@@ -1,5 +1,88 @@
 import os, random, math
 import classes
-
+from classes import User, SavingsAccount, Database
 print("Who are you")
-user_name = input("")
+# user_name = input("")
+users_array = []
+user_name1 = "Maximo Gutierrez"
+user1 = User(user_name1)
+user1.create_account(23.4)
+users_array.append(user1)
+user_name2 = "Enrique Gutierrez"
+user2 = User(user_name2)
+user2.create_account(34.4)
+users_array.append(user2)
+user_name3 = "Ricardo Arjona"
+user3 = User(user_name3)
+user3.create_account(33.2)
+users_array.append(user3)
+user_name4 = "Bobby Pulido"
+user4 = User(user_name4)
+user4.create_account(14.2)
+users_array.append(user4)
+user_name5 = "Areidi Leal"
+user5 = User(user_name5)
+user5.create_account(80.2)
+users_array.append(user5)
+
+database = Database(users_array)
+#initial_deposit = float(input())
+os.system('cls')
+print("Welcome to MaxBank!")
+print("What is your name?")
+user_name = str(input())
+exist_user = False
+for user in database.get_users():
+    counter = 0
+    if user.name == user_name:
+        print("Welcome back, ",user_name,"!")
+        final_user = database.get_users()[counter]
+        exist_user = True
+        break
+    counter +=1
+if not exist_user:
+    print("Creating new account")
+    final_user = User(user_name)
+    print("How much will you deposit?")
+    first_deposit = float(input())
+    final_user.create_account(first_deposit)
+
+menu_controller = 0
+while menu_controller != 4:
+    print("--------------------------------------")
+    print("Logged as: ",final_user.name)
+    print("What would you like to do?")
+    print("1. Watch accounts")
+    print("2. Create new account")
+    print("3. Deposit to an account")
+    print("4. Exit")
+    menu_controller = int(input())
+    match menu_controller:
+        case 1:
+            print("Watching accounts...")
+            final_user.info()
+        case 2:
+            print("Creating new account...")
+            print("How much would you like to deposit to your new account?")
+            initial_deposit2 = float(input())
+            final_user.create_account(initial_deposit2)
+        case 3:
+            print("Depositing money to an account")
+            print("Introduce your account number:", end=' ')
+            account_number = float(input())
+            exist_account = False
+            counter2 = 0
+            for account in final_user.get_accounts():
+                if account_number ==  account.account_number:
+                    exist_account = True
+                    print("How much would you like to deposit?")
+                    deposit = float(input())
+                    account.deposit(deposit)
+                    print("Deposit succeded")
+                    account.info()
+            if not exist_account:
+                print("Account not found, sorry.")
+
+        case 4:
+            break
+        
